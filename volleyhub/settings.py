a94ceb343c63ba9f75da54c4306bca8102477c3f
Omar Obeid <ohobeid@gmail.com>
2026-04-14 00:57:31 +0300

    feat(auth): add login/logout via django.contrib.auth (#1)
    - Wire django.contrib.auth.urls under /accounts/
    - LOGIN_URL / LOGIN_REDIRECT_URL / LOGOUT_REDIRECT_URL in settings
    - Styled login template matching the glassmorphism theme
    - Login/Logout dock items in base nav (POST form for logout per Django 5+)
    
    Closes #1

--- volleyhub/settings.py
+++ volleyhub/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Authentication redirects
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'player_list'
+LOGOUT_REDIRECT_URL = 'login'
